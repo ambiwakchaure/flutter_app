cd892d66a04005af1e4b305734c1e95d73c2618d
GokulWakchaure <gokulwakchaure19@gmail.com>
2020-04-23 11:15:50 +0530

    hello world app design

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 //this is the liabrary which contain all material widgets
 import 'package:flutter/material.dart';
-
 //declaire the main method : entry point of our app
 void main() {
     //purpose of this method is inflate the widgets and attach to the screen
     //it will show whatever we pass parameter
     runApp(
-        Center(//to center widget on screen
-            child: Text(
-                "Hello Flutter",
-                textDirection: TextDirection.ltr
-            ),
+        MaterialApp( // latest version of dart so new keyword is optional
+            title: "My Flutter App",//app name
+            home: Scaffold(
+                appBar: AppBar(title: Text("My First App screen"),),
+                body: Material(
+                    color: Colors.lightBlueAccent,//color of app
+                    child: Center(//to center widget on screen
+                        child: Text(
+                            "Hello Flutter",
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(color: Colors.white,fontSize: 40.0),//apply style to the text
+                        ),
+                    ),
+                ),
+            )//it is allow to place app bar on top of the screen
         )
     );
 }
