6b3a955e0da384ceca46475716e0da56a3db6666
GokulWakchaure <gokulwakchaure19@gmail.com>
2020-04-23 13:46:05 +0530

    container widgets

---
+++ lib/app_screens/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        color: Colors.deepPurple,
+        //width: 200.0,
+        //height: 100.0,
+        //margin: EdgeInsets.all(15.0),
+        //padding: EdgeInsets.all(15.0),
+        //margin: EdgeInsets.only(right : 15.0,left: 15.0,top: 15.0,bottom: 15.0),
+        //padding: EdgeInsets.only(right : 15.0,left: 15.0,top: 15.0,bottom: 15.0),
+        child: Text("Flight", textDirection: TextDirection.ltr),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,12 @@
-//this is the liabrary which contain all material widgets
 import 'package:flutter/material.dart';
-import './app_screens/first_screen.dart';
-//declaire the main method : entry point of our app
-void main() => runApp(MyFlutterApp());// function expression : In dart if we have 1 line of code for calling so we use
-    //=>
-    //purpose of this method is inflate the widgets and attach to the screen
-    //it will show whatever we pass parameter
-
-//StatelessWidget : The widget that will be return in this class will not contain any state
-//and it will never change in future
-class MyFlutterApp extends StatelessWidget
+import './app_screens/home.dart';
+void main()
 {
-  @override
-  Widget build(BuildContext context)
-  {
-    return MaterialApp( // latest version of dart so new keyword is optional
-      debugShowCheckedModeBanner: false,//remove debug tag from screen
-        title: "My Flutter App",//app name
-        home: Scaffold(
-          appBar: AppBar(title: Text("My First App screen"),),
-          body: FirstScreen(),
-        )//it is allow to place app bar on top of the screen
+    runApp(
+        MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: "Exploring UI Widgets",
+          home: Home(),
+        )
     );
-  }
-
-
 }
