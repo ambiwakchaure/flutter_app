f3dbb8e90c0f3e950cd0b7419686fd80fe9aa78b
GokulWakchaure <gokulwakchaure19@gmail.com>
2020-04-24 15:19:32 +0530

    floating button snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,10 @@ void main() {
           title:
               Text("Long Listview", style: TextStyle(fontFamily: 'Raleway'))),
       body: getListView(),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+          tooltip: "Add one more item"),
     ),
   ));
 }
@@ -16,6 +20,18 @@ void main() {
 List<String> getListElements() {
   var items = List<String>.generate(1000, (counter) => "Items $counter");
   return items;
+}
+
+void showSnackbar(BuildContext context, String item) {
+  var snackBar = SnackBar(
+    content: Text("You just tapped item $item"),
+    action: SnackBarAction(
+        label: "UNDO",
+        onPressed: () {
+          //perform action
+        }),
+  );
+  Scaffold.of(context).showSnackBar(snackBar);
 }
 
 Widget getListView() {
@@ -26,6 +42,9 @@ Widget getListView() {
       trailing: Icon(Icons.arrow_back),
       leading: Icon(Icons.landscape),
       title: Text(listItems[index], style: TextStyle(fontFamily: 'Raleway')),
+      onTap: () {
+        showSnackbar(context, listItems[index]);
+      },
     );
   });
   return listView;
