d4577c38ceca456f7316e9973b79f38d98738cd0
GokulWakchaure <gokulwakchaure19@gmail.com>
2020-04-24 16:27:09 +0530

    stateful app widgets

--- lib/main.dart
+++ lib/main.dart
@@ -3,49 +3,51 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: "Exploring UI Widgets",
-    home: Scaffold(
-      appBar: AppBar(
-          title:
-              Text("Long Listview", style: TextStyle(fontFamily: 'Raleway'))),
-      body: getListView(),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
-          tooltip: "Add one more item"),
-    ),
+    title: "Stateful App Example",
+    home: FavoriteCity(),
   ));
 }
 
-List<String> getListElements() {
-  var items = List<String>.generate(1000, (counter) => "Items $counter");
-  return items;
+class FavoriteCity extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _FavoriteCityState();
+  }
 }
 
-void showSnackbar(BuildContext context, String item) {
-  var snackBar = SnackBar(
-    content: Text("You just tapped item $item"),
-    action: SnackBarAction(
-        label: "UNDO",
-        onPressed: () {
-          //perform action
-        }),
-  );
-  Scaffold.of(context).showSnackBar(snackBar);
-}
+class _FavoriteCityState extends State<FavoriteCity> {
+  String nameCity = "";
 
-Widget getListView() {
-  var listItems = getListElements();
-  var listView = ListView.builder(itemBuilder: (context, index) {
-    //item builder loads only visible element into the memory other hides it so its a memory efficient
-    return ListTile(
-      trailing: Icon(Icons.arrow_back),
-      leading: Icon(Icons.landscape),
-      title: Text(listItems[index], style: TextStyle(fontFamily: 'Raleway')),
-      onTap: () {
-        showSnackbar(context, listItems[index]);
-      },
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stateful App Example"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onSubmitted: (String userInput) {
+                setState(() {
+                  nameCity = userInput;
+                });
+              },
+              /*onChanged: (String userInput) {
+                setState(() {
+                  nameCity = userInput;
+                });
+              },*/
+            ),
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Text("Your bext city is $nameCity",
+                  style: TextStyle(fontSize: 20.0)),
+            )
+          ],
+        ),
+      ),
     );
-  });
-  return listView;
+  }
 }
